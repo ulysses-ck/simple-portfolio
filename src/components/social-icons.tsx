4a5777f9d8e43c6f3194b095ae549aa3b6c56bac
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-03 13:59:07 -0300

    refactor: replace text labels with React icons in SocialIcons component for improved visual consistency

--- src/components/social-icons.tsx
+++ src/components/social-icons.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link";
+import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 
 export default function SocialIcons() {
   return (
-    <div className="flex gap-4">
-      <div>Github</div>
-      <div>Twitter</div>
-      <div>LinkedIn</div>
+    <div className="flex gap-4 items-center">
+      <FaGithub className="w-6 h-6 text-dark-content hover:text-dark-text dark:text-light-content dark:hover:text-light-text" />
+      <FaTwitter className="w-6 h-6 text-dark-content hover:text-dark-text dark:text-light-content dark:hover:text-light-text" />
+      <FaLinkedin className="w-6 h-6 text-dark-content hover:text-dark-text dark:text-light-content dark:hover:text-light-text" />
     </div>
   );
 }
