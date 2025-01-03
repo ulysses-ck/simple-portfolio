e8b0ef385383dedd0de74beb267f263647af6cf2
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-03 11:15:35 -0300

    feat: decouple content from html
    also bring correct colors from design

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,17 @@
+import { navData } from "@/data/nav-data";
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center">
-      <ul className="flex gap-4">
-        <li>Home</li>
-        <li>About</li>
-        <li>Tech Stack</li>
-        <li>Projects</li>
-        <li>Contact</li>
+      <ul className="flex gap-8">
+        {navData.map((item) => (
+          <li key={item.id}>
+            <Link href={item.link} className="text-dark-text dark:text-light-text">
+              {item.name}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
---
+++ src/data/nav-data.ts
@@ -0,0 +1,7 @@
+export const navData = [
+  { id: "1-home", name: "Home", link: "/" },
+  { id: "2-about", name: "About", link: "/about" },
+  { id: "3-tech-stack", name: "Tech Stack", link: "/tech-stack" },
+  { id: "4-projects", name: "Projects", link: "/projects" },
+  { id: "5-contact", name: "Contact", link: "/contact" },
+];
