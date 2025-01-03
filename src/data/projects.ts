417062efd85c9b927beb8ab33febc9a43c2028d3
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-03 20:01:13 -0300

    feat: add projects data structure for project listings with titles, descriptions, images, and links

---
+++ src/data/projects.ts
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "Project 1",
+    description: "This is a sample description random things are here in description. This is a sample project lorem ipsum generator for dummy content",
+    image: "https://placecats.com/200/300",
+    linkPreview: "https://example.com",
+    linkGithub: "https://github.com/example/project",
+  },
+  {
+    title: "Project 2",
+    description: "This is a sample description random things are here in description. This is a sample project lorem ipsum generator for dummy content",
+    image: "https://placecats.com/300/200",
+    linkPreview: "https://example.com",
+    linkGithub: "https://github.com/example/project",
+  },
+  {
+    title: "Project 3",
+    description: "This is a sample description random things are here in description. This is a sample project lorem ipsum generator for dummy content",
+    image: "https://placecats.com/400/200",
+    linkPreview: "https://example.com",
+    linkGithub: "https://github.com/example/project",
+  },
+  {
+    title: "Project 4",
+    description: "This is a sample description random things are here in description. This is a sample project lorem ipsum generator for dummy content",
+    image: "https://placecats.com/500/300",
+    linkPreview: "https://example.com",
+    linkGithub: "https://github.com/example/project",
+  },
+  {
+    title: "Project 5",
+    description: "This is a sample description random things are here in description. This is a sample project lorem ipsum generator for dummy content",
+    image: "https://placecats.com/300/200",
+    linkPreview: "https://example.com",
+    linkGithub: "https://github.com/example/project",
+  },
+  {
+    title: "Project 6",
+    description: "This is a sample description random things are here in description. This is a sample project lorem ipsum generator for dummy content",
+    image: "https://placecats.com/400/200",
+    linkPreview: "https://example.com",
+    linkGithub: "https://github.com/example/project",
+  },
+];
