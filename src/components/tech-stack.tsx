02863d44967cbcba42689ef4f58f020d7b1653c7
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-03 13:23:04 -0300

    refactor: update tech stack component to use new data structure with icons and colors
    - Enhanced the TechStack component to utilize a more structured data format for tech stack items, including icon components and color properties.
    - Improved styling for better visual presentation and accessibility.

--- src/components/tech-stack.tsx
+++ src/components/tech-stack.tsx
@@ -2,16 +2,18 @@ import { techStack } from "@/data/tech-stack";
 export default function TechStack() {
 
   return (
-    <section className="flex flex-col items-center justify-center">
-      <h2>Tech Stack</h2>
-      <p>Technologis I've been working with recently</p>
+    <section className="flex flex-col items-center justify-center gap-4 my-10">
+      <h2 className="text-4xl font-bold text-dark-text dark:text-light-text">Tech Stack</h2>
+      <p className="text-lg text-gray-500">Technologis I've been working with recently</p>
       <div className="w-full grid grid-cols-6 gap-4">
-        {techStack.map((item) => (
-          <div key={item.name} className="flex flex-col items-center justify-center">
-            {item.icon}
-            <p>{item.name}</p>
-          </div>
-        ))}
+        {techStack.map(({ name, Icon, color }) => {
+          return (
+            <div key={name} className="flex flex-col items-center justify-center gap-2 bg-gray-100 dark:bg-gray-800 p-4 rounded-md">
+              <Icon className="w-10 h-10" color={color} colorRendering={"#000"} />
+              <p className="text-sm text-gray-500">{name}</p>
+            </div>
+          )
+        })}
       </div>
     </section>
   );
--- src/data/tech-stack.tsx
+++ src/data/tech-stack.tsx
@@ -2,17 +2,17 @@ import { FaReact, FaHtml5, FaCss3, FaJs, FaGithub, FaGit, FaSass, FaBootstrap } 
 import { SiRedux } from "react-icons/si";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { VscVscode } from "react-icons/vsc";
-
-export const techStack = [
-  { name: "HTML", icon: <FaHtml5 /> },
-  { name: "CSS", icon: <FaCss3 /> },
-  { name: "JS", icon: <FaJs /> },
-  { name: "REACT", icon: <FaReact /> },
-  { name: "REDUX", icon: <SiRedux /> },
-  { name: "BOOTSTRAP", icon: <FaBootstrap /> },
-  { name: "TAILWIND", icon: <RiTailwindCssFill /> },
-  { name: "SASS", icon: <FaSass /> },
-  { name: "GIT", icon: <FaGit /> },
-  { name: "VSCODE", icon: <VscVscode /> },
-  { name: "GITHUB", icon: <FaGithub /> },
+import type { IconType } from "react-icons";
+export const techStack: { name: string, Icon: IconType, color: string }[] = [
+  { name: "HTML", Icon: FaHtml5, color: "#E34F26" },
+  { name: "CSS", Icon: FaCss3, color: "#1572B6" },
+  { name: "JS", Icon: FaJs, color: "#F7DF1E" },
+  { name: "REACT", Icon: FaReact, color: "#61DAFB" },
+  { name: "REDUX", Icon: SiRedux, color: "#764ABC" },
+  { name: "BOOTSTRAP", Icon: FaBootstrap, color: "#563D7C" },
+  { name: "TAILWIND", Icon: RiTailwindCssFill, color: "#38BDF8" },
+  { name: "SASS", Icon: FaSass, color: "#CC6699" },
+  { name: "GIT", Icon: FaGit, color: "#F05032" },
+  { name: "VSCODE", Icon: VscVscode, color: "#007ACC" },
+  { name: "GITHUB", Icon: FaGithub, color: "#f0f6fc" },
 ];
