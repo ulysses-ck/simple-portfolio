e271ca3a7baf9ad1f7b7d7c94ed16912da741f5c
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-04 00:17:19 -0300

    feat: add Header and Footer components to RootLayout for improved site structure and navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import { ThemeProvider } from "next-themes";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +32,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider attribute={"class"} defaultTheme="system" enableSystem>
+        <Header />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
