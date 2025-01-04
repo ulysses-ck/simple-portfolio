637a40951ed8b25b89c24e1519a82b116c7ce9d7
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-03 21:26:09 -0300

    feat: integrate GradientText component in Header for enhanced branding display

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import GradientText from "./gradient-text";
 import Navbar from "./navbar";
 import SocialIcons from "./social-icons";
 import ThemeToggle from "./theme-toggle";
@@ -5,8 +6,8 @@ import ThemeToggle from "./theme-toggle";
 export default function Header() {
   return (
     <header className="flex justify-between items-center my-4">
-      <h1 className="text-2xl font-bold text-blue-500 dark:text-red-300">
-        Brand
+      <h1 className="text-2xl font-bold">
+        <GradientText>Pavan MG</GradientText>
       </h1>
       <Navbar />
       <SocialIcons />
