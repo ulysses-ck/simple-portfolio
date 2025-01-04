a0d17d0f07085dbfb90b57d53a7263abca1c6157
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-03 21:26:02 -0300

    style: update Footer component layout and enhance text with GradientText for improved visual appeal and structure

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,22 +1,25 @@
+import GradientText from "./gradient-text";
 import Navbar from "./navbar";
 import SocialIcons from "./social-icons";
 
 export default function Footer() {
   return (
-    <footer>
-      <div>
+    <footer className="flex flex-col gap-4 mb-10">
+      <div className="flex justify-between items-center">
         <h3>Brand</h3>
-        <div>
-          <span>+91 1234567890</span>
-          <span>info@example.com</span>
+        <div className="flex gap-8">
+          <div className="flex gap-4">
+            <span>+91 1234567890</span>
+            <span>info@example.com</span>
+          </div>
+          <SocialIcons />
         </div>
-        <SocialIcons />
       </div>
-      <div>
+      <hr className="w-full border-black dark:border-white" />
+      <div className="flex justify-between items-center">
         <Navbar />
-        <p>Designed and built by Pavan MG with Love & Coffee</p>
+        <p>Designed and built by <GradientText>Pavan MG</GradientText> with <GradientText>Love</GradientText> & <GradientText>Coffee</GradientText></p>
       </div>
     </footer>
   );
 }
-
