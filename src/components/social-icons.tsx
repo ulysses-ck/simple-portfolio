252e4597a369faf787c893aa13563f1c602520d1
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-04 01:51:50 -0300

    feat: refactor SocialIcons component to dynamically render icons from social data

--- src/components/social-icons.tsx
+++ src/components/social-icons.tsx
@@ -1,11 +1,14 @@
-import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
+import { social } from "@/data/social";
 
 export default function SocialIcons() {
   return (
     <div className="flex gap-4 items-center">
-      <FaGithub className="w-6 h-6 text-dark-content hover:text-dark-text dark:text-light-content dark:hover:text-light-text" />
-      <FaTwitter className="w-6 h-6 text-dark-content hover:text-dark-text dark:text-light-content dark:hover:text-light-text" />
-      <FaLinkedin className="w-6 h-6 text-dark-content hover:text-dark-text dark:text-light-content dark:hover:text-light-text" />
+      {social.map((item) => (
+        <Link href={item.url} key={item.url}>
+          <item.Icon className="w-6 h-6 text-dark-content hover:text-dark-text dark:text-light-content dark:hover:text-light-text" />
+        </Link>
+      ))}
     </div>
   );
 }
---
+++ src/data/social.ts
@@ -0,0 +1,16 @@
+import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
+
+export const social = [
+  {
+    url: "https://github.com/pavangm",
+    Icon: FaGithub,
+  },
+  {
+    url: "https://twitter.com/pavangm",
+    Icon: FaTwitter,
+  },
+  {
+    url: "https://www.linkedin.com/in/pavangm",
+    Icon: FaLinkedin,
+  },
+];
