7c27b59b6f4b02222f324dd77c78ce754e4ed345
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-04 00:17:29 -0300

    refactor: remove unused components from Home page for cleaner structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
 import Hero from "@/components/hero";
-import Projects from "@/components/projects";
-import TechStack from "@/components/tech-stack";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <main className="px-44 flex flex-col">
-      <Header />
       <Hero />
-      <TechStack />
-      <Projects />
-      <Footer />
     </main>
   );
 }
