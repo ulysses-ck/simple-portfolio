694c535cfc48441382e98fc4d45bb85e63b538df
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-03 21:05:08 -0300

    style: enhance Projects component with updated heading and paragraph styles for improved readability and visual appeal

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -3,8 +3,8 @@ import CardProjects from "./card-projects";
 export default function Projects() {
   return (
     <section className="my-4 flex flex-col gap-4 items-center">
-      <h2 className="text-2xl font-bold text-blue-500 dark:text-red-300">Projects</h2>
-      <p>Things I've built so far</p>
+      <h2 className="text-4xl font-bold text-dark-text dark:text-light-text">Projects</h2>
+      <p className="text-lg text-gray-500">Things I've built so far</p>
       <div className="grid grid-cols-3 gap-4 items-center w-full">
         <CardProjects />
       </div>
