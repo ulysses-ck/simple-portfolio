e1c47d2d9c6e4286330d8c60d6f978a1120bd20a
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-04 00:28:40 -0300

    style: add padding to RootLayout body for improved layout and spacing consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased px-44`}
       >
         <ThemeProvider attribute={"class"} defaultTheme="system" enableSystem>
         <Header />
