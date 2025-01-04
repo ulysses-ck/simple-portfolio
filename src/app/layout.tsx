9c5fe760b71a4efea12ea11285e190a91a131b07
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-04 01:33:04 -0300

    style: update RootLayout to use flexbox for main content layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       >
         <ThemeProvider attribute={"class"} defaultTheme="system" enableSystem>
           <Header />
-          <main className="flex-grow">{children}</main>
+          <main className="flex-grow flex">{children}</main>
           <Footer />
         </ThemeProvider>
       </body>
