834273714113bce16a4399f58164adac0a70b11e
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-04 00:51:53 -0300

    feat: add education and experience data files to provide structured information for user profiles

---
+++ src/data/education.ts
@@ -0,0 +1,8 @@
+export const education = [
+  {
+    title: "Bachelor in Electrical & Communications",
+    institution: "Bangalore Institute of Technology",
+    startDate: "2015-08-01",
+    endDate: "2020-12-01",
+  },
+];
---
+++ src/data/experience.ts
@@ -0,0 +1,26 @@
+export const experience = [
+  {
+    title: "Junior Web Developer",
+    company: "Dr. Rajkumar's Learning App",
+    ubication: "Bengaluru",
+    startDate: "2021-09-01",
+    endDate: "2021-12-01",
+    typeWork: "Full Time",
+  },
+  {
+    title: "Web Developer Intern",
+    company: "IonPixelz Web Solutions",
+    ubication: "Bengaluru",
+    startDate: "2021-09-01",
+    endDate: "2021-12-01",
+    typeWork: "Internship",
+  },
+  {
+    title: "SEO / SEM Specialist",
+    company: "HAAPS",
+    ubication: "Bengaluru",
+    startDate: "2021-09-01",
+    endDate: "2021-12-01",
+    typeWork: "Internship",
+  },
+];
