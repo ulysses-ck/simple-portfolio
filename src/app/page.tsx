ea3e3b1564fa59888afcad7ef53e1e435381f84f
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-04 00:39:22 -0300

    refactor: simplify Home component by removing unnecessary wrapper and returning Hero directly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import Hero from "@/components/hero";
 
 export default function Home() {
-  return (
-    <main className="px-44 flex flex-col">
-      <Hero />
-    </main>
-  );
+  return <Hero />;
 }
