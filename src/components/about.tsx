1cfcbb22c807b67404d87920047dd24937337f40
ulysses-ck <ulisesapaza.280@gmail.com>
2025-01-04 01:17:36 -0300

    feat: create About component to showcase application overview, work experience, and education sections

---
+++ src/components/about.tsx
@@ -0,0 +1,30 @@
+import Education from "./education";
+import WorkExperience from "./work-experience";
+
+export default function About() {
+  return (
+    <section className="flex flex-col gap-8 my-10">
+      <div>
+        <h2 className="text-2xl font-semibold text-dark-text">About</h2>
+        <p className="text-dark-content">
+          The Generator App is an online tool that helps you to export
+          ready-made templates ready to work as your future website. It helps
+          you to combine slides, panels and other components and export it as a
+          set of static files: HTML/CSS/JS.
+        </p>
+      </div>
+      <div>
+        <h2 className="text-2xl font-semibold text-dark-text">
+          Work Experience
+        </h2>
+        <div className="flex flex-col gap-4">
+          <WorkExperience />
+        </div>
+      </div>
+      <div>
+        <h2 className="text-2xl font-semibold text-dark-text">Education</h2>
+        <Education />
+      </div>
+    </section>
+  );
+}
